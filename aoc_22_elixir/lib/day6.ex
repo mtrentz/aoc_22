45dbf1b0f96ee960d33ab3d55ca500eb59a074cb
mtrentz <mateus.trentz@gmail.com>
2022-12-10 00:26:12 -0300

    day6

---
+++ aoc_22_elixir/lib/day6.ex
@@ -0,0 +1,84 @@
+defmodule DaySix do
+  def all_unique(list) do
+    length(list) == length(Enum.uniq(list))
+  end
+
+  def solve_pt1 do
+    resultado =
+      File.read!("input/day6.txt")
+      |> String.codepoints()
+      |> Enum.reduce(
+        %{
+          buffer: [],
+          index: 0,
+          found: false
+        },
+        fn letter, acc ->
+          cond do
+            # If not enough length, just add to buffer and increment index
+            length(acc.buffer) < 4 ->
+              %{buffer: acc.buffer ++ [letter], index: acc.index + 1, found: false}
+
+            # If all unique and not yet found, add to buffer, increment index and set found to true
+            all_unique(acc.buffer) and not acc.found ->
+              %{buffer: acc.buffer, index: acc.index, found: true}
+
+            # If yet not found, remove first from buffer, add new element and increment index
+            acc.found == false ->
+              %{
+                buffer: Enum.slice(acc.buffer, 1, 3) ++ [letter],
+                index: acc.index + 1,
+                found: false
+              }
+
+            # Default, does nothing
+            true ->
+              acc
+          end
+        end
+      )
+
+    IO.inspect(resultado)
+  end
+
+  def solve_pt2 do
+    resultado =
+      File.read!("input/day6.txt")
+      |> String.codepoints()
+      |> Enum.reduce(
+        %{
+          buffer: [],
+          index: 0,
+          found: false
+        },
+        fn letter, acc ->
+          cond do
+            # If not enough length, just add to buffer and increment index
+            length(acc.buffer) < 14 ->
+              %{buffer: acc.buffer ++ [letter], index: acc.index + 1, found: false}
+
+            # If all unique and not yet found, add to buffer, increment index and set found to true
+            all_unique(acc.buffer) and not acc.found ->
+              %{buffer: acc.buffer, index: acc.index, found: true}
+
+            # If yet not found, remove first from buffer, add new element and increment index
+            acc.found == false ->
+              %{
+                buffer: Enum.slice(acc.buffer, 1, 13) ++ [letter],
+                index: acc.index + 1,
+                found: false
+              }
+
+            # Default, does nothing
+            true ->
+              acc
+          end
+        end
+      )
+
+    IO.inspect(resultado)
+  end
+end
+
+DaySix.solve_pt1()
+DaySix.solve_pt2()
